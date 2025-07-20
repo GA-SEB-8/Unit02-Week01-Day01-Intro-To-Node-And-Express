d89b79823773820b884984b9f93aef21f2c20683
omarakamal <omaraakamal@gmail.com>
2025-07-20 13:23:13 +0300

    update

---
+++ class-code/server.js
@@ -0,0 +1,12 @@
+// 1. npm init -y: initializes my application as a node application
+
+// 2. npm install express
+
+// npm install name-of-package
+
+// to import package
+
+const colors = require('colors')
+const express = require("express")
+
+console.log(colors.rainbow("Welcome to my Appliation"))
