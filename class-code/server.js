464e69bffd187bdff7f67731ff02ab428302d503
omarakamal <omaraakamal@gmail.com>
2025-07-20 13:36:24 +0300

    update

--- class-code/server.js
+++ class-code/server.js
@@ -6,6 +6,10 @@
 
 // 4. const app = express()
 
+// 5. app.listen(3000,()=>{
+// console.log("Listening on port 3000")
+// })
+
 
 
 
@@ -17,3 +21,10 @@ const express = require("express") //importing express package
 
 const app = express() // creates a express application
 
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+})
